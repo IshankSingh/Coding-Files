23abb6c08e5fe3fae119fcc681a0384f6ed79197
Ishank Singh <singhishank2303@gmail.com>
2019-07-23 17:00:02 +0530

    Initial commit

---
+++ binaryToDecimalWithGreatestConsecutiveOnes.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    int n, num;
+    cout << "Enter the number " << endl;
+    cin >> num;
+
+    std::vector<int> v;
+    while (num > 0)
+    {
+        int i = num % 2;
+        v.push_back(i);
+        num = num / 2;
+    }
+    // while (!v.empty())
+    // {
+    //     cout << v.back();
+    //     v.pop_back();
+    // }
+
+    int count = 0, total = 0;
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        cout << v[i];
+        if (v[i] == 1)
+        {
+            count++;
+        }
+        if (count > total)
+        {
+            total = count;
+        }
+        if (v[i] == 0)
+        {
+            count = 0;
+        }
+    }
+    cout << "\nMaximum consecutive one's are " << total;
+}
