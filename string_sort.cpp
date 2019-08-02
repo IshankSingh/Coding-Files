6ca2a6464c9bb25106f710b7aa21326318642c5f
Ishank Singh <singhishank2303@gmail.com>
2019-08-02 15:58:37 +0530

    Changes to be committed:

--- string_sort.cpp
+++ string_sort.cpp
@@ -10,7 +10,6 @@ int main(void)
     string str;
     cout << "Enter the text\n";
     cin >> str;
-
     transform(str.begin(), str.end(), str.begin(), ::tolower);
     sort(str.begin(), str.end());
     cout << str;
